f29e7e07253bd32465b1e41aba9a235e42ebc809
vguttmann <vincent.guttmann@gmx.de>
2024-05-07 09:25:32 +0200

    Changed CANMessage to a valid C struct

--- CANMessage.h
+++ CANMessage.h
@@ -19,24 +19,24 @@
 
 //----------------------------------------------------------------------------------------------------------------------
 
-class CANMessage {
-public: uint32_t id = 0;  // Frame identifier
-public: bool ext = false; // false -> standard frame, true -> extended frame
-public: bool rtr = false; // false -> data frame, true -> remote frame
-public: uint8_t idx = 0;  // This field is used by the driver
-public: uint8_t len = 0;  // Length of data (0 ... 8)
-public: union {
-    uint64_t data64; // Caution: subject to endianness
-    int64_t  data_s64; // Caution: subject to endianness
-    uint32_t data32[2]; // Caution: subject to endianness
-    int32_t  data_s32[2]; // Caution: subject to endianness
-    float    dataFloat[2]; // Caution: subject to endianness
-    uint16_t data16[4]; // Caution: subject to endianness
-    int16_t  data_s16[4]; // Caution: subject to endianness
-    int8_t   data_s8[8];
-    uint8_t  data[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
-};
-};
+typedef struct {
+    uint32_t id = 0;  // Frame identifier
+    bool ext = false; // false -> standard frame, true -> extended frame
+    bool rtr = false; // false -> data frame, true -> remote frame
+    uint8_t idx = 0;  // This field is used by the driver
+    uint8_t len = 0;  // Length of data (0 ... 8)
+    union {
+        uint64_t data64; // Caution: subject to endianness
+        int64_t  data_s64; // Caution: subject to endianness
+        uint32_t data32[2]; // Caution: subject to endianness
+        int32_t  data_s32[2]; // Caution: subject to endianness
+        float    dataFloat[2]; // Caution: subject to endianness
+        uint16_t data16[4]; // Caution: subject to endianness
+        int16_t  data_s16[4]; // Caution: subject to endianness
+        int8_t   data_s8[8];
+        uint8_t  data[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+    };
+} CANMessage;
 
 //----------------------------------------------------------------------------------------------------------------------
 
