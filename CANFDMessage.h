e6feb64375c4a97b17bcaea94a63477da15c834b
vguttmann <vincent.guttmann@gmx.de>
2024-05-08 15:05:39 +0200

    Change pad function to C

--- CANFDMessage.h
+++ CANFDMessage.h
@@ -84,34 +84,7 @@ public: union {
       //   Methods
       //·············································································
 
-public: void pad(void) {
-    uint8_t paddedLength = len;
-    if ((len > 8) && (len < 12)) {
-        paddedLength = 12;
-    }
-    else if ((len > 12) && (len < 16)) {
-        paddedLength = 16;
-    }
-    else if ((len > 16) && (len < 20)) {
-        paddedLength = 20;
-    }
-    else if ((len > 20) && (len < 24)) {
-        paddedLength = 24;
-    }
-    else if ((len > 24) && (len < 32)) {
-        paddedLength = 32;
-    }
-    else if ((len > 32) && (len < 48)) {
-        paddedLength = 48;
-    }
-    else if ((len > 48) && (len < 64)) {
-        paddedLength = 64;
-    }
-    while (len < paddedLength) {
-        data[len] = 0;
-        len += 1;
-    }
-}
+
 
       //·············································································
 
@@ -121,7 +94,37 @@ public: void pad(void) {
 
 };
 
-bool isValid(CANFDMessage& message) {
+
+void pad(CANFDMessage* message) {
+    uint8_t paddedLength = len;
+    if ((message->len > 8) && (message->len < 12)) {
+        paddedLength = 12;
+    }
+    else if ((message->len > 12) && (message->len < 16)) {
+        paddedLength = 16;
+    }
+    else if ((message->len > 16) && (message->len < 20)) {
+        paddedLength = 20;
+    }
+    else if ((message->len > 20) && (message->len < 24)) {
+        paddedLength = 24;
+    }
+    else if ((message->len > 24) && (message->len < 32)) {
+        paddedLength = 32;
+    }
+    else if ((message->len > 32) && (message->len < 48)) {
+        paddedLength = 48;
+    }
+    else if ((message->len > 48) && (message->len < 64)) {
+        paddedLength = 64;
+    }
+    while (message->len < paddedLength) {
+        data[message->len] = 0;
+        message->len += 1;
+    }
+}
+
+bool isValid(CANFDMessage* message) {
     if ((message->type == CAN_REMOTE) || (message->type == CAN_DATA)) { // Remote frame
         return message->len <= 8;
     }
